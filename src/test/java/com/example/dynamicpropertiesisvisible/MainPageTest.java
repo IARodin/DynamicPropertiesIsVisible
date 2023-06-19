221cbcee0cfa5901067d5b31dcaa7370608ed709
IARodin <71449805+IARodin@users.noreply.github.com>
2023-06-19 16:07:02 +0700

    Initial commit

---
+++ src/test/java/com/example/dynamicpropertiesisvisible/MainPageTest.java
@@ -0,0 +1,41 @@
+package com.example.dynamicpropertiesisvisible;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void isVisible() {
+        driver.get("https://demoqa.com/dynamic-properties");
+
+        WebElement visibleAfter = driver.findElement(By.cssSelector("#visibleAfter"));
+        assertTrue(visibleAfter.isDisplayed(), "Не отображается элемент");
+    }
+}
